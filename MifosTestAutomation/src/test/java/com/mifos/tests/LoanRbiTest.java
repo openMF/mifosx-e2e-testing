e55a4da624221a88dcf6423249aecba3c13b20e8
sughosh88 <sughosh@confluxtechnologies.com>
2016-02-10 18:27:21 +0530

    test

--- MifosTestAutomation/src/test/java/com/mifos/tests/LoanRbiTest.java
+++ MifosTestAutomation/src/test/java/com/mifos/tests/LoanRbiTest.java
@@ -7,7 +7,6 @@ import cucumber.api.junit.Cucumber;
 @Cucumber.Options(format = { "pretty", "html:target/LoanRBI/cucumber-html-report",
 		"json-pretty:target/LoanRBI/cucumber-json-report.json" },
 		features = { "src/test/resources/features/test.feature" },
-		tags = { "@scenario25-2350-RBI-EI-DB-DL-REC-NOCOM-RNI-CTPD-DL-MD-TR-1-ONTIME-BLR" },
 		glue = { "com.mifos.steps" })
 
 public class LoanRbiTest {
